3d30eef0ed015b93084664505304a7afe9e716b5
Michael Xu <michaelxu12345@gmail.com>
2020-06-14 23:29:50 -0500

    Made some changes/additions to grid class

--- MeshlessPoisson/grid.cpp
+++ MeshlessPoisson/grid.cpp
@@ -1,9 +1,8 @@
 #include "grid.h"
 #include <stdexcept>
-#include <Eigen/Dense>
 #include "math.h"
 #include <algorithm>
-Grid::Grid(vector<std::tuple<double, double, double>> points, vector<std::tuple<vector<int>, int, double>> boundaries, int rbfExp, int polyDeg) {
+Grid::Grid(vector<std::tuple<double, double, double>> points, vector<std::tuple<vector<int>, int, vector<double>>> boundaries, int rbfExp, int polyDeg, int stencilSize, double omega) {
 	points_ = points;
 	boundaries_ = boundaries;
 	rbfExp_ = rbfExp;
@@ -11,7 +10,10 @@ Grid::Grid(vector<std::tuple<double, double, double>> points, vector<std::tuple<
 	laplaceMatSize_ = (int)(points.size());
 	int numPoint = (int)(points.size());
 	values_ = new Eigen::VectorXd(numPoint);
-	laplaceMat_ = new Eigen::MatrixXd(numPoint, numPoint);
+	laplaceMat_ = new Eigen::SparseMatrix<double>(numPoint, numPoint);
+	stencilSize_ = stencilSize;
+	omega_ = omega;
+	bcFlags_ = std::vector<int>(numPoint);
 }
 Grid::~Grid() {
 	delete values_;
@@ -22,25 +24,59 @@ double distance(std::tuple<double, double, double> refPoint, std::tuple<double, 
 	return std::sqrt(std::get<0>(refPoint)*std::get<0>(queryPoint) + std::get<1>(refPoint)*std::get<1>(queryPoint) + std::get<2>(refPoint)*std::get<2>(queryPoint));
 }
 
-void Grid::setBC(int bNum, std::string type, double boundValue) {
+void Grid::setBCFlag(int bNum, std::string type, vector<double> boundValues) {
 	if (!type.compare("dirichlet") && !type.compare("neumann")) {
 		throw std::invalid_argument("Specified Boundary Condition on Boundary #" + std::to_string(bNum) + " is not dirichlet or neumann");
 	}
-	std::tuple<vector<int>, int, double> bound = boundaries_.at(bNum);
-	std::get<1>(bound) = type.compare("dirichlet") ? 0 : 1;
-	std::get<2>(bound) = boundValue;
-	if (type.compare("dirichlet")) {
-		for (size_t i = 0; i < std::get<0>(bound).size(); i++) {
-			(*values_)(std::get<0>(bound).at(i)) = boundValue;
+	std::tuple<vector<int>, int, vector<double>> bound = boundaries_.at(bNum);
+	std::get<1>(bound) = type.compare("dirichlet") ? 1 : 2; 
+	for (size_t i = 0; i < std::get<0>(bound).size(); i++) {
+		bcFlags_[std::get<0>(bound)[i]] = std::get<1>(bound);
+	}
+	std::get<2>(bound) = boundValues;
+}
+
+void Grid::boundaryOp() {
+	for (size_t i = 0; i < boundaries_.size(); i++) {
+		//dirichlet
+		if (std::get<1>(boundaries_[i]) == 1){
+			for (size_t j = 0; j < std::get<0>(boundaries_[i]).size(); j++) {
+				(*values_)(std::get<0>(boundaries_[i]).at(j)) = std::get<2>(boundaries_[i]).at(j);
+			}
+		}
+		//implement neumann later.
+	}
+}
+
+//Builds Sparse Matrix that approximates laplacian operator for SOR from the interpolation weights.
+void Grid::build_laplacian(int k) {
+	vector<Eigen::Triplet<double>> tripletList;
+	for (int i = 0; i < laplaceMatSize_; i++) {
+		std::pair <Eigen::VectorXd, vector<int>> weights = laplaceWeights(i);
+		for (size_t j = 0; j < weights.second.size(); j++) {
+			tripletList.push_back(Eigen::Triplet<double>(i, weights.second[j], weights.first(j)));
+		}
+	}
+	laplaceMat_->setFromTriplets(tripletList.begin(), tripletList.end());
+}
+
+void Grid::sor(int numIts, Eigen::VectorXd source) {
+	for (int it = 0; it < numIts; it++) {
+		// set/enforce the boundary conditions
+		boundaryOp();
+		for (int i = 0; i < laplaceMatSize_; i++) {
+			if (bcFlags_[i] != 0) {
+				continue;
+			}
+			double x_i = (1-omega_)*(*values_)(i) + omega_/laplaceMat_->coeff(i,i)*source(i);
+			for (int j = 0; j < laplaceMatSize_; j++) {
+				x_i -= j == i ? 0 : laplaceMat_->coeff(i, j)*(*values_)(j);
+			}
 		}
 	}
 }
-void Grid::build_laplacian() {
-
-}
-
 //Fix distance function if we want to extend code to 3D. Makes and sorts a distance array with int point IDs. Brute force, so O(n log n) time
-vector<int> Grid::kNearestNeighbors(int pointID, int k) {
+vector<int> Grid::kNearestNeighbors(int pointID) {
 	vector<std::pair<double, int>> distances;
 	std::tuple<double, double, double> refPoint = points_[pointID];
 	for (size_t i = 0; i < points_.size(); i++) {
@@ -51,20 +87,21 @@ vector<int> Grid::kNearestNeighbors(int pointID, int k) {
 	std::sort(distances.begin(), distances.end());
 
 	vector<int> nearestNeighbors;
-	for (int i = 1; i <= k; i++) {
+	//Have to include point itself in the stencil since otherwise diag would be zeros.
+	for (int i = 0; i < stencilSize_; i++) {
 		nearestNeighbors.push_back(distances[i].second);
 	}
 	return nearestNeighbors;
 }
 //Builds PHS coefficients matrix for a point
 //returns pair with first = coeff matrix and second = list of neighbors, to save time.
-std::pair<Eigen::MatrixXd, vector<int>> Grid::buildCoeffMatrix(int pointID, int k) {
+std::pair<Eigen::MatrixXd, vector<int>> Grid::buildCoeffMatrix(int pointID) {
 	int polyTerms = (polyDeg_ + 1)*(polyDeg_ + 2) / 2;
-	vector<int> neighbors = kNearestNeighbors(pointID, k);
+	vector<int> neighbors = kNearestNeighbors(pointID);
 	//build A-matrix
-	Eigen::MatrixXd coeff_mat = Eigen::MatrixXd(k + polyTerms, k + polyTerms);
-	for (int i = 0; i < k; i++) {
-		for (int j = 0; j < k; j++) {
+	Eigen::MatrixXd coeff_mat = Eigen::MatrixXd(stencilSize_ + polyTerms, stencilSize_ + polyTerms);
+	for (int i = 0; i < stencilSize_; i++) {
+		for (int j = 0; j < stencilSize_; j++) {
 			double r = distance(points_[neighbors[i]], points_[neighbors[j]]);
 			coeff_mat(i, j) = std::pow(r, rbfExp_);
 			/*
@@ -75,8 +112,8 @@ std::pair<Eigen::MatrixXd, vector<int>> Grid::buildCoeffMatrix(int pointID, int 
 	}
 	//fill P-matrix sections.
 	int colIndex;
-	for (int row = 0; row < k; row++) {
-		colIndex = k;
+	for (int row = 0; row < stencilSize_; row++) {
+		colIndex = stencilSize_;
 		for (int p = 0; p <= polyDeg_; p++) {
 			for (int q = 0; q <= p; q++) {
 				double x = std::get<0>(points_.at(neighbors[row]));
@@ -92,21 +129,21 @@ std::pair<Eigen::MatrixXd, vector<int>> Grid::buildCoeffMatrix(int pointID, int 
 }
 
 //Builds RHS and solves for the laplace weights for a point
-std::pair<Eigen::VectorXd, vector<int>> Grid::laplaceWeights (int pointID, int k) {
-	std::pair<Eigen::MatrixXd, vector<int>> coeffs = buildCoeffMatrix(pointID, k);
+std::pair<Eigen::VectorXd, vector<int>> Grid::laplaceWeights (int pointID) {
+	std::pair<Eigen::MatrixXd, vector<int>> coeffs = buildCoeffMatrix(pointID);
 	vector<int> neighbors = coeffs.second;
 	int polyTerms = (polyDeg_ + 1)*(polyDeg_ + 2) / 2;
-	Eigen::VectorXd rhs = Eigen::VectorXd(k + polyTerms);
+	Eigen::VectorXd rhs = Eigen::VectorXd(stencilSize_ + polyTerms);
 	//Build RHS
 	double xEval = std::get<0>(points_[pointID]);
 	double yEval = std::get<1>(points_[pointID]);
-	for (int i = 0; i < k; i++) {
+	for (int i = 0; i < stencilSize_; i++) {
 		double delX = std::get<0>(points_[neighbors[i]]) - xEval;
 		double delY = std::get<1>(points_[neighbors[i]]) - yEval;
 		rhs(i) = rbfExp_ * rbfExp_*std::pow(delX*delX + delY * delY, (double)(rbfExp_) / 2.0 - 1);
 	}
 	//poly terms
-	int rowIndex = k;
+	int rowIndex = stencilSize_;
 	for (int p = 0; p <= polyDeg_; p++) {
 		for (int q = 0; q <= p; q++) {
 			double laplacePoly = 0;
--- MeshlessPoisson/grid.h
+++ MeshlessPoisson/grid.h
@@ -5,24 +5,30 @@
 #include <tuple>
 #include <string>
 #include <Eigen/Dense>
+#include <Eigen/Sparse>
 using std::vector;
 class Grid {
 public:
-	Grid(vector<std::tuple<double, double, double>> points, vector<std::tuple<vector<int>, int, double>> boundaries, int rbfExp, int polyDeg);
+	Grid(vector<std::tuple<double, double, double>> points, vector<std::tuple<vector<int>, int, vector<double>>> boundaries, int rbfExp, int polyDeg, int stencilSize, double omega);
 	~Grid();
-	void setBC(int boundary, std::string type, double boundValue);
-	void build_laplacian();
-	vector <int> kNearestNeighbors(int pointNumber, int k);
+	void setBCFlag(int boundary, std::string type, vector<double> boundValue);
+	void boundaryOp();
+	void build_laplacian(int k);
+	void sor(int numIts, Eigen::VectorXd source);
+	vector <int> kNearestNeighbors(int pointNumber);
 private:
 	vector<std::tuple<double, double, double>> points_;
-	vector<std::tuple<vector<int>, int, double>> boundaries_;
+	vector<std::tuple<vector<int>, int, vector<double>>> boundaries_;
 	int rbfExp_;
 	int polyDeg_;
 	int laplaceMatSize_;
+	int stencilSize_;
+	vector<int> bcFlags_;
+	double omega_;
 	Eigen::VectorXd* values_;
-	Eigen::MatrixXd* laplaceMat_;
+	Eigen::SparseMatrix<double>* laplaceMat_;
 
-	std::pair<Eigen::MatrixXd, vector<int>> buildCoeffMatrix(int pointNum, int k);
-	std::pair<Eigen::VectorXd, vector<int>> laplaceWeights(int pointID, int k);
+	std::pair<Eigen::MatrixXd, vector<int>> buildCoeffMatrix(int pointNum);
+	std::pair<Eigen::VectorXd, vector<int>> laplaceWeights(int pointID);
 };
 #endif
